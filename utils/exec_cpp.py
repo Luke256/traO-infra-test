3922070fcb9a24248c798f5ad284930ad8814607
Luke256 <yuta.k.200628@gmail.com>
2024-11-12 00:34:11 +0900

    ascii

--- utils/exec_cpp.py
+++ utils/exec_cpp.py
@@ -7,7 +7,7 @@ def exec_cpp(mx_log):
     subprocess.run(["g++", "main.cpp", "-o", "main", "-D", f"MX_LOG={mx_log}"])
     
     times = []
-    for _ in tqdm.tqdm(range(ITERATIONS)):
+    for _ in tqdm.tqdm(range(ITERATIONS), ascii=True):
         start = time.perf_counter()
         subprocess.run(["./main"], stdout=subprocess.DEVNULL)
         end = time.perf_counter()
