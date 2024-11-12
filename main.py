26e7116ec67f91889bcaec31283777411b809d40
Luke256 <yuta.k.200628@gmail.com>
2024-11-12 14:22:25 +0900

    modify

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from utils.test import run_test
 from utils.send2traq import send2traq
+from config import TEST_NAME
 
 if __name__ == '__main__':
-    print("Webhook test")
-    send2traq("Ping")
+    send2traq(f"計測開始: {TEST_NAME}")
 
     run_test()
